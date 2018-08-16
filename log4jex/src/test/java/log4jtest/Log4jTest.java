cd539628d208df35e953bd15ae5b4b005a98b064
Debasis Mishra <E004246@cigniti.com>
2018-08-16 17:45:13 +0530

    JacobEx

--- log4jex/src/test/java/log4jtest/Log4jTest.java
+++ log4jex/src/test/java/log4jtest/Log4jTest.java
@@ -5,6 +5,11 @@ import log4jex.Log4jEx;
 import org.junit.Assert;
 import org.junit.Test;
 
+
+/*
+ * For further info read below article
+ * https://dzone.com/articles/log4j-2-configuration-using-properties-file
+*/	
 public class Log4jTest {
 
 	@Test
